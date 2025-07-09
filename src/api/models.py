bd25554c429d0eb41671b95186252ca17d8d54d4
ZeqeB <85196913+ezebellino@users.noreply.github.com>
2025-07-09 19:08:43 -0300

    first commit

--- src/api/models.py
+++ src/api/models.py
@@ -1,10 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__= 'user'
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
@@ -16,4 +17,22 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+        
+    tasks = relationship('Task', back_populates='user')
+    
+class Task(db.Model):
+    __tablename__= 'task'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    label: Mapped[str] = mapped_column(String(256), nullable=False)
+    completed: Mapped[bool] = mapped_column(Boolean(), default=False, nullable=False)
+    user_id: Mapped[int] =  mapped_column(Integer, ForeignKey('user.id'), nullable=False)
+    
+    user = relationship('User', back_populates='tasks')
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "label": self.label,
+            "completed": self.completed
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,4 +1,4 @@
-"""
+f"""
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
